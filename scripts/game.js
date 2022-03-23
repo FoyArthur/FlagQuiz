3d132dcafd3a44ffe322b0fa9e2e3c3972517ef3
Arthur <iamarthurfoy@gmail.com>
2022-03-22 23:47:18 -0700

    prototype

--- scripts/game.js
+++ scripts/game.js
@@ -14,38 +14,56 @@ for(let i = 0; i < 5; i++) {
 }
 
 const Nums = Array.from(set);
-for(x of Nums) {
-    console.log(x);
-}
+
+// for(const item in flags) {
+//     console.log(item);
+//     var img = document.createElement("img");
+//     img.src = "https://flagcdn.com/h240/"+ item +".png"
+//     document.getElementById("flag").appendChild(img);
+//     var g = document.createElement("div");
+//     g.innerHTML = flags[item];
+//     document.getElementById("flag").appendChild(g);
+// }
 
 let arr2 = [];
 for(let i = 0; i < 5; i++) {
     var img = document.createElement("img");
     img.src = "https://flagcdn.com/h240/"+ arr[Nums[i]]+".png"
     arr2.push(img);
-    console.log(arr2[i])
 }
 
 let button = document.getElementById("submit");
 let text = document.getElementById("guess");
+
 let score = document.getElementById("score");
 let times = document.getElementById("times");
 let num = 0;
 document.getElementById("flag").appendChild(arr2[0]);
 let idx = 0;
 
+let form = document.getElementById("form");
+
+form.addEventListener('submit', function(event) {
+    // if(event.code === "Enter") {
+        event.preventDefault();
+        button.click();
+    // }
+});
+
 button.addEventListener("click", () => {
     if(text.value === "") {
         alert("You must submit text");
     } else {
         if(idx < 4) {
-            document.getElementById("flag").removeChild(arr2[idx]);
+            document.getElementById("flag").removeChild(arr2[idx]);    
+            console.log((flags[arr[Nums[idx]]]).toLowerCase());
+            if((text.value).toLowerCase() == ((flags[arr[Nums[idx]]]).toLowerCase())) {
+                score.innerHTML = parseInt(score.innerHTML) + 1;
+                console.log("YeS");
+            }
             idx++;
             document.getElementById("flag").appendChild(arr2[idx]);
-            console.log(text.value);
-            if(text.value == "a") {
-                score.innerHTML = parseInt(score.innerHTML) + 1;
-            }
+            
             times.innerHTML = parseInt(times.innerHTML) +1;
             text.value = "";
         } else {
@@ -63,6 +81,8 @@ button.addEventListener("click", () => {
     
     }
 });
+
+
 
 
 
